99a0fd20f3e9628595f00c723d7bf69831db90fb
Ala2Adel <ala2.alaraby@gmail.com>
2021-07-04 16:45:40 +0200

    design home page WIP

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:meal_monkey/Widgets/restaurants_card.dart';
 import 'package:meal_monkey/localization/language_constants.dart';
 
 import '../app_colors.dart';
@@ -15,6 +16,14 @@ class _HomePageState extends State<HomePage> {
   String _selectedLocation;
 
   var _locations = ['Cairo', 'Alexandria', 'Giza'];
+  List _images = [
+    "assets/offers.png",
+    "assets/pizza.png",
+    "assets/western.png",
+    "assets/sri_lanka.png",
+    "assets/indian.png"
+  ];
+  List _labels = ["Offers", "Italian", "Western", "Sri Lankan", "Indian"];
 
   @override
   Widget build(BuildContext context) {
@@ -123,7 +132,7 @@ class _HomePageState extends State<HomePage> {
                   child: Padding(
                     padding: const EdgeInsetsDirectional.only(start: 20),
                     child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
+                      // crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         SvgPicture.asset(
                           "assets/search.svg",
@@ -144,6 +153,74 @@ class _HomePageState extends State<HomePage> {
                       ],
                     ),
                   ),
+                ),
+              ),
+              Container(
+                  height: _height * 0.15,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: _images.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsetsDirectional.only(
+                              end: 12, top: 10),
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  child: Image.asset(
+                                    _images[index],
+                                    height: 80,
+                                    width: _width * 0.26,
+                                    fit: BoxFit.fill,
+                                  )),
+                              Container(
+                                  child: Padding(
+                                      padding: const EdgeInsets.only(top: 7.0),
+                                      child: Center(
+                                        child: Text(_labels[index],
+                                            style: TextStyle(
+                                                fontSize: 14.0,
+                                                color: AppColors.darkGrey,
+                                                fontFamily: "Metropolis",
+                                                fontWeight: FontWeight.bold)),
+                                      )))
+                            ],
+                          ),
+                        );
+                      })),
+              Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      getTranslated(context, "pop_rest"),
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: AppColors.darkGrey,
+                          fontFamily: "Metropolis",
+                          fontWeight: FontWeight.w900),
+                    ),
+                    Text(
+                      getTranslated(context, "view_all"),
+                      style: TextStyle(
+                          fontSize: 13.0,
+                          color: AppColors.orange,
+                          fontFamily: "Metropolis",
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: _height*0.4,
+                child: ListView.builder(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  itemCount: 4,
+                  itemBuilder: (BuildContext context, int index) {
+                    return RestaurantsCard();
+                  },
                 ),
               ),
             ],
